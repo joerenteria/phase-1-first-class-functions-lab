443ce3b9e84777707aa6d55e03484969175920ce
Joe Renteria <joe@electronflight.com>
2021-07-23 08:55:25 -0700

    commit tree 7f9f99ecb32bc48fef2665a4179e9f33be2a1235

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
-// Code your solution in this file!
+const returnFirstTwoDrivers = function(names){
+
+    return names.slice(0,2);
+      
+  }
+  
+
+const returnLastTwoDrivers = function(names){
+
+    return names.slice(2);
+      
+  }
+
+
+const selectingDrivers = [returnFirstTwoDrivers,returnLastTwoDrivers];
+
+
+
+function createFareMultiplier(fare){
+    return function() {
+        return fare * fare;
+      }  
+}
+
+const fareDoubler = function(fare){
+    return function() {
+        return fare * fare;
+      } 
+}
+
+const fareTripler = function(fare){
+    return function() {
+        return fare * 3;
+      }  
+}
+
+function selectDifferentDrivers(names,operation){
+    return operation;
+    
+    }
+  
+
